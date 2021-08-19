9ade728e9c652a069fccb3b4257fa7e606cf4904
zhangjilei <zhangjilei@iqihang.com>
2021-08-19 18:04:08 +0800

    preview-img 属性

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -8,13 +8,13 @@ export default function Index() {
   function tapImg() {
     console.log('点击图片')
   }
-  // const htmlContent = '<span style="color: red">我是红色文本</span><img src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"/>'
+  const htmlContent = '<span style="color: red">我是红色文本</span><img src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"/>'
 
-  const htmlContent = `<span style="color: red">我是红色文本</span><img src="${excellentIcon}"/>`
+  // const htmlContent = `<span style="color: red">我是红色文本</span><img src="${excellentIcon}"/>`
 
   return (
     <View className='index'>
-      <mp-html content={htmlContent} onImgtap={tapImg} />
+      <mp-html content={htmlContent} onImgtap={tapImg} preview-img={false} />
     </View>
   )
 }
