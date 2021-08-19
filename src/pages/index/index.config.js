d7d0d44911eb98978d9b3a2d0ed0869340ec9092
zhangjilei <zhangjilei@iqihang.com>
2021-08-19 15:43:08 +0800

    init

---
+++ src/pages/index/index.config.js
@@ -0,0 +1,6 @@
+export default {
+  navigationBarTitleText: '首页',
+  usingComponents: {
+    'mp-html': '../../components/mp-weixin/index'
+  }
+}
---
+++ src/pages/index/index.jsx
@@ -0,0 +1,20 @@
+import { View } from '@tarojs/components'
+import excellentIcon from '../../assets/images/excellentIcon.png'
+import './index.scss'
+
+
+export default function Index() {
+
+  function tapImg() {
+    console.log('点击图片')
+  }
+  // const htmlContent = '<span style="color: red">我是红色文本</span><img src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"/>'
+
+  const htmlContent = `<span style="color: red">我是红色文本</span><img src="${excellentIcon}"/>`
+
+  return (
+    <View className='index'>
+      <mp-html content={htmlContent} onImgtap={tapImg} />
+    </View>
+  )
+}
